eebfdedd6852ab590296f29ec1302fb1e298f6fb
myungkeun <mkro0616@gmail.com>
2021-05-07 02:05:26 +0900

    modify pyaction command test

--- src/test/java/com/mklee/accountutils/CorptaxSorter/service/CorptaxSorterServiceTest.java
+++ src/test/java/com/mklee/accountutils/CorptaxSorter/service/CorptaxSorterServiceTest.java
@@ -1,8 +1,6 @@
 package com.mklee.accountutils.CorptaxSorter.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.IOException;
